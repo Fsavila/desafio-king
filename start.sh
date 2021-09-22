e984e89e1b766673cec2ef7895ad94ce2eee0af3
bramos013 <brunoramos013@gmail.com>
2021-09-22 19:48:13 -0300

    Correcao executa start.sh

--- start.sh
+++ start.sh
@@ -6,19 +6,11 @@ echo 'Fique tranquilo, estarei realizando os procedimentos para você.'
 sleep 1
 
 echo 'Iniciando Terraform'
-cd ./terraform
-terraform apply --auto-approve
+cd terraform/ ; terraform init >/dev/null 2>&1 ; terraform apply --auto-approve
 sleep 5
 
 echo 'Iniciando Ansible'
 cd ../ansible/ ; ansible-playbook -i hosts main.yml -u bruno -b
-sleep 1
-
-echo 'Pronto Finalizando Stage'
-echo ''
-echo 'Seu Container do GitLab está rodando no endereço ip_da_vm:5000'
-echo ''
-echo 'Para obter a senha root do Gitlab, basta rodar o comando: sudo docker exec gitlab cat /etc/gitlab/initial_root_password'
-sleep 3
+sleep 5
 
 echo 'Obrigado!'

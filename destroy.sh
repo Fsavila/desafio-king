dc213812a084c997150697ec3f3520dd251d2917
bramos013 <brunoramos013@gmail.com>
2021-09-22 19:48:42 -0300

    Correcao executa destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,13 +1,13 @@
 #Rodar com cuidado esse arquivo
 #!/bin/sh
-
+cd terraform/ ; terraform init >/dev/null 2>&1
 echo 'Olá, Você realmente deseja apagar sua infraestrutura montada [S/N]?'
 read escolha
 
 if [ $escolha == S ]
 then
     echo 'Desmontando Infraestrutura'
-    cd ./terraform ; terraform destroy --auto-approve ; echo [webserver] > ../ansible/hosts
+    terraform destroy --auto-approve ; echo [webserver] > ../ansible/hosts    terraform destroy --auto-approve ; echo [webserver] > ../ansible/hosts
 exit 3
 
 else

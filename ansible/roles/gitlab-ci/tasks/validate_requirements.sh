f41becce3658c1f34a63df00299472dc3fd55d4c
bramos013 <brunoramos013@gmail.com>
2021-09-22 18:13:15 -0300

    Upload playbooks do servidor

---
+++ ansible/roles/gitlab-ci/tasks/validate_requirements.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo 'Realizando verificações de requisitos!'
+echo ''
+
+terraform --version > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+    echo "Terraform Instalado..."
+    terraform --version
+    sleep 2
+else
+    echo "Que pena! Você não possui o Terraform instalado, para instalar visite o README, disponibilizei um link para facilitar a instalação"
+    exit 3
+fi    
+
+ansible --version > /dev/null 2>&1
+
+echo ""
+
+if [ $? -eq 0 ]
+then
+    echo "Ansible Instalado..."
+    ansible --version
+    sleep 2
+else
+    echo "Que pena! Você não possui o Ansible instalado, para instalar visite o README, disponibilizei um link para facilitar a instalação"
+    exit 3
+fi
+
+echo ""
+echo "Parabéns, seu computador possui os requisitos técnicos, execute o comando: ./start.sh"
+sleep 3
